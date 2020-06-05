896069f68ed1202f064b5a451f8cee7ef915a677
BaptisteAsselin <baptiste.asselin@orange.fr>
2020-06-05 20:24:32 +0200

    implementation du systeme de chemin pour l'IA version alpha

--- Scripts/navigation.cs
+++ Scripts/navigation.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class navigation : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] path = new GameObject[4];
-    [SerializeField] private UnityEngine.AI.NavMeshAgent agent = null;
-    private GameObject target = null;
+    [SerializeField] private NavMeshAgent agent = null;
+    private int target = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent.SetDestination(path[this.target].transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if()
+        if(Vector3.Distance(this.transform.position, this.path[this.target].transform.position) < 3) {
+            this.target = (++this.target) % path.Length;
+            agent.SetDestination(path[this.target].transform.position);
+        }
     }
 }
