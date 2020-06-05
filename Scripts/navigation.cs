e0db360915e71c38a3ff389963e01794b96a4989
BaptisteAsselin <baptiste.asselin@orange.fr>
2020-06-05 19:50:11 +0200

    debug 19:50

---
+++ Scripts/navigation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navigation : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] path = new GameObject[4];
+    [SerializeField] private UnityEngine.AI.NavMeshAgent agent = null;
+    private GameObject target = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
