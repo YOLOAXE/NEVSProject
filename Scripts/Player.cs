8461c9ee388da7ec27de109327a357a628f2b41e
BaptisteAsselin <baptiste.asselin@orange.fr>
2020-06-06 16:48:00 +0200

    implementation du systeme des bots

---
+++ Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float noise = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float getNoise() {
+        return this.noise;
+    }
+}
--- Scripts/navigation.cs
+++ Scripts/navigation.cs
@@ -7,20 +7,102 @@ public class navigation : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] path = new GameObject[4];
+    [SerializeField] private GameObject[] players = null;
     [SerializeField] private NavMeshAgent agent = null;
-    private int target = 0;
+    private int patrolTarget = 0;
+    private GameObject target = null;
+    [SerializeField] private float targetingRange = 10f;
+    [SerializeField] private LayerMask lm = 0;
     // Start is called before the first frame update
     void Start()
     {
-        agent.SetDestination(path[this.target].transform.position);
+        agent.SetDestination(path[this.patrolTarget].transform.position);
+        StartCoroutine(viewUpdate());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position, this.path[this.target].transform.position) < 3) {
-            this.target = (++this.target) % path.Length;
-            agent.SetDestination(path[this.target].transform.position);
+        if(!this.asTarget()) {
+            this.agent.isStopped = false;
+            this.patrol();
+        } else {
+            this.agent.isStopped = true;
+            Debug.Log(this.target);
+            if(this.isOnSight()) {
+                Debug.Log("attaque");
+                //attaque
+            } else {
+                Debug.Log("chercher");
+                this.find();
+            }
         }
+    }
+
+    private void patrol() {
+        if(Vector3.Distance(this.transform.position, this.path[this.patrolTarget].transform.position) < 2) {
+            this.patrolTarget = (++this.patrolTarget) % path.Length;
+            agent.SetDestination(path[this.patrolTarget].transform.position);
+        }
+    }
+
+    IEnumerator viewUpdate() {
+        while(true) {
+            this.target = this.proximityCheck();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private GameObject proximityCheck() {
+
+        float actualRange = Vector3.Distance(this.transform.position, this.players[0].transform.position) / this.players[0].GetComponent<Player>().getNoise();
+        float range = 0;
+        GameObject potentialTarget = this.players[0];
+
+        foreach(GameObject player in players) {
+            range = Vector3.Distance(this.transform.position, player.transform.position) / player.GetComponent<Player>().getNoise();
+            if(actualRange > range) {
+
+                actualRange = range;
+                potentialTarget = player;
+            }
+        }
+        Debug.Log(Vector3.Distance(this.transform.position, potentialTarget.transform.position));
+
+        if(actualRange <= this.targetingRange) {
+            return potentialTarget;
+        } else {
+            return null;
+        }
+    }
+
+    private bool asTarget() {
+        if(this.target == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private bool isOnSight() {
+        RaycastHit hit;
+        Vector3 fromPosition = this.transform.position;
+        Vector3 toPosition = this.target.transform.position;
+        Vector3 direction = toPosition - fromPosition;
+
+
+        if(Physics.Raycast(fromPosition,direction,out hit, Mathf.Infinity, lm))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.yellow);
+            if(hit.collider.gameObject == this.target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void find() {
+        agent.isStopped = false;
+        agent.SetDestination(target.transform.position);
     }
 }
