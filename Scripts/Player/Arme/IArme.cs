6096117e0b7ce3e47f737c3708cd5ea600baf795
Albarell <Albarell>
2020-06-09 20:08:58 +0200

    20:08

--- Scripts/Player/Arme/IArme.cs
+++ Scripts/Player/Arme/IArme.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IArme : MonoBehaviour
+public interface IArme 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
