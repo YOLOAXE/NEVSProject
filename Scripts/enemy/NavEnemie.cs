6a6a003e3eb8bfcedb94fdc966a670865c0a56dc
BaptisteAsselin <baptiste.asselin@orange.fr>
2020-07-06 15:26:18 +0200

    qqdfbqdfb

--- Scripts/enemy/NavEnemie.cs
+++ Scripts/enemy/NavEnemie.cs
@@ -6,19 +6,21 @@ using Mirror;
 
 namespace VHS
 {
-    public class NavEnemie : NetworkBehaviour
+    public abstract class NavEnemie : NetworkBehaviour
     {
         [Header("EnemieInfo")]
-        [SerializeField] private NavMeshAgent agent = null;
+        [SerializeField] protected NavMeshAgent agent = null;
         [SyncVar] public float currentHealth = 100.0f;
         [SerializeField] private float maxHealth = 100.0f;
 
         [Header("Deplacement")]       
         [SerializeField] private List<GameObject> path = new List<GameObject>();
-        [SerializeField] private float chaseDistance = 30f;
-        [SerializeField] private float attaqueDistance = 15f;
+        [SerializeField] protected float huntingDistance = 10f;
+        [SerializeField] protected float attaqueDistance = 15f;
         [SerializeField] private float pathChangeDistance = 2f;
-        [SerializeField] private int patrolTarget = 0;
+        [SerializeField] protected int patrolTarget = 0;
+        [SerializeField] private bool research = false;
+        [SerializeField] private int researchNb = 3;
         private GameObject target = null;
 
         [Header("Detection")]
@@ -26,7 +28,7 @@ namespace VHS
 
         [Header("Combat")]
         [SerializeField] private bool inFight = false;
-        [SerializeField] private GameObject targetPlayer = null;
+        [SerializeField] protected GameObject targetPlayer = null;
         [SerializeField] private float timeFight = 15f;
         private float timerF = 0f;
 
@@ -42,11 +44,16 @@ namespace VHS
             if(!isServer){return;}
             patrol();
             TargetPlayerTime();
+            if(this.inFight)
+            {
+                this.attack();
+            }
+            
         }
 
         private void patrol()
         {
-            if (Vector3.Distance(this.transform.position, this.path[this.patrolTarget].transform.position) < pathChangeDistance && !inFight)
+            if (Vector3.Distance(this.transform.position, this.path[this.patrolTarget].transform.position) < pathChangeDistance && !inFight && !this.research)
             {
                 this.patrolTarget = ((this.patrolTarget+1)%path.Count);
                 agent.SetDestination(path[this.patrolTarget].transform.position);
@@ -63,20 +70,36 @@ namespace VHS
             {
                 this.timerF = -20;
                 this.inFight = false;
+                this.research = true;
                 this.ed.ResetTP();
                 this.targetPlayer = null;
                 agent.isStopped = false;
+                agent.SetDestination(new Vector3(Random.Range(-huntingDistance, huntingDistance), this.transform.position.y, this.transform.position.z + Random.Range(-huntingDistance, huntingDistance)));
                 agent.SetDestination(path[this.patrolTarget].transform.position);
+                
+            } 
+            
+            if (this.research && this.agent.desiredVelocity.magnitude > 0)
+            {
+                agent.SetDestination(new Vector3(this.transform.position.x + Random.Range(-huntingDistance, huntingDistance), this.transform.position.y, this.transform.position.z + Random.Range(-huntingDistance, huntingDistance)));
+                this.researchNb--;
+                if (this.researchNb == 0)
+                {
+                    this.research = false;
+                    this.researchNb = 3;
+                }
             }
         }
 
         public void addTargetPlayer(GameObject TPlayer)
         {
+            Debug.Log("loop");
             this.inFight = true;
-            agent.isStopped = true;
             this.timerF = this.timeFight;
             this.targetPlayer = TPlayer;
         }
+
+        public virtual void attack() { }
         
         #endregion
     }
---
+++ Scripts/enemy/Soldier.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Mirror;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.AI;
+
+namespace VHS
+{
+    public class Soldier : NavEnemie
+    {
+        Vector3 lookPos = Vector3.zero;
+        Quaternion lookAngle = Quaternion.identity;
+        #region Start & Stop Callbacks
+        public override void attack()
+        {
+            if (Vector3.Distance(this.transform.position, base.targetPlayer.transform.position) > base.huntingDistance)
+            {
+                base.agent.isStopped = false;
+                base.agent.SetDestination(new Vector3(Random.Range(-base.huntingDistance, base.huntingDistance), 0, Random.Range(-base.huntingDistance, base.huntingDistance)));
+            }
+            base.agent.SetDestination(base.targetPlayer.transform.position);
+
+            lookPos = this.targetPlayer.transform.position - this.transform.position;
+            lookPos.y = 0;
+            lookAngle = Quaternion.LookRotation(lookPos);
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, lookAngle, Time.deltaTime * (base.agent.angularSpeed/10));
+        }
+
+        #endregion
+    }
+}
+
